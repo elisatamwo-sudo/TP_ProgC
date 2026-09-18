a5f2db33f03810e69043a55f9456207bb31fcc13
elisatamwo-sudo <elisatamwo@gmail.com>
2026-09-18 13:38:28 +0000

    test

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <string.h>
 
+int main (){
+printf ("char : %zu octet (s)\n" , sizeof (char));
+printf("singned char : %zu octet (s)\n" , sizeof (signed char));
+printf ("unsigned char : %zu octet (s)\n" , sizeof (unsigned char));
+
+printf(" short : %zu octet (s)\n" , sizeof (short));
+printf ("unsigned short : %zu octet (s)\n" , sizeof (unsigned short));
+
+printf(" int : %zu octet (s)\n" , sizeof (int));
+printf ("unsigned int : %zu octet (s)\n" , sizeof (unsigned int));
+
+printf(" long int : %zu octet (s)\n" , sizeof (long int));
+printf ("unsigned long int : %zu octet (s)\n" , sizeof (unsigned long int));
+
+printf(" long  long int : %zu octet (s)\n" , sizeof (long long int));
+printf ("unsigned  long long int : %zu octet (s)\n" , sizeof (unsigned long long int));
+
+printf(" float : %zu octet (s)\n" , sizeof (float));
+
+
+printf(" double : %zu octet (s)\n" , sizeof (double));
+
+
+printf(" long double : %zu octet (s)\n" , sizeof (long double));
+
+
+return 0;
+
+}
